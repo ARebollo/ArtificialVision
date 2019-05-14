6b5419ede4303d14e0ceb1962edcd4357f17b59d
Antonio <muaddib58@gmail.com>
2019-05-14 12:27:55 +0200

    Ajam.jpg

--- Practica 4/mainwindow.py
+++ Practica 4/mainwindow.py
@@ -147,17 +147,22 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
                     #This should set the piece of grayImageDest to the correct value. Maybe move outside to increase efficiency.
                     #Use imgRegions and the regionID to set each point to the correct value, that way it's only one big loop instead
                     #of many smaller overlapping ones
+                        avgGrey = newRegion.returnAverage()
+                        for k in range (rect[0], rect[0] + rect[2], 1):
+                            for l in range(rect[1], rect[1] + rect[3], 1):
+                                if self.imgRegions[l][k] == regionID:
+                                    self.grayImageDest[l][k] = avgGrey
                     
 
                     #print(regionID)
                     regionID += 1
                     #self.mask = cv2.copyMakeBorder(self.edges, 1,1,1,1, cv2.BORDER_CONSTANT, value = 255)
-
+        '''
         for i in range(240):
             for i in range(320):
                 self.grayImageDest[i][j] = regionList[self.imgRegions[i][j]+1].avgGrey
 
-
+        '''
         checkBreak = False
         if self.checkBoxBorders.isChecked() is True:
             #We skip the first to avoid out of bounds. Can be done manually, or adding an if check that makes everything slow as fuck.
